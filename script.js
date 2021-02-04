c0194929feb10a0bf28c43b35fffb23e096dd916
Russell <russelldev@protonmail.com>
2021-02-04 13:31:28 +0000

    Add delay to writeText

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const textEl = document.getElementById('text');
 const speedEl = document.getElementById('speed');
 const text = 'We Love Problem Solving';
 let index = 1;
-let speed = 300 / speedEl.value;
+let speed = 300 / 5;
+// speedEl.value
 
 writeText();
+
+
 
 function writeText() {
     textEl.innerText = text.slice(0,index);
     index = index + 1;
-    if (index > text.length) {
-        index = 1;
-    }
+    if (index === text.length) {
+        setTimeout(() => {
+            index = 0;
+        }, 1500);
+    }     
     setTimeout(writeText, speed);
 }
 speedEl.addEventListener('input', (e) => speed = 300 / e.target.value);
