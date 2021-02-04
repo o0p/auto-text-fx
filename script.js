fcc8d8c3bd8d0b6d11781468d0f1895c6397c141
Russell <russelldev@protonmail.com>
2021-02-04 07:51:39 +0000

    Add selevtor vars

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'We Love Problem Solving';
+let index = 1;
+let speed = 300 / speedEl.value;
 
