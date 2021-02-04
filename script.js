f92d9599e5a7c3a77e57d77ab23b054c8dcbed12
Russell <russelldev@protonmail.com>
2021-02-04 07:55:42 +0000

    Add writeText func

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const text = 'We Love Problem Solving';
 let index = 1;
 let speed = 300 / speedEl.value;
 
+writeText();
+
+function writeText() {
+    textEl.innerText = text.slice(0,index);
+    index = index + 1;
+    if (index > text.length) {
+        index = 1;
+    }
+    setTimeout(writeText, speed);
+}
